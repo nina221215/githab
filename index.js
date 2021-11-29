ad501ad81168186329d96a18bd3daf3cced55032
sala 305 <sala305@poliacademico.edu.loc>
2021-11-29 07:51:00 -0500

    Se añade codigo al index

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const chalk = require('chalk')
-const fuentes=require('chalk')
+const express=require('express')
+const app=express()
+app.listen(3000,()=>{ 
+    console.log('Servidor Corriendo')
+})
+
 console.log(((chalk.blue)("Hola","Mundo","Irreal")))
