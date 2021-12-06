64e01c256a63159584ab4d72cfaac41d0ffad7b2
sala 305 <sala305@poliacademico.edu.loc>
2021-12-06 07:52:02 -0500

    Creacion de metodos get y post

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const chalk = require('chalk')
-const express=require('express')
-const app=express()
-app.listen(3000,()=>{ 
-    console.log('Servidor Corriendo')
+
+console.log(chalk.green('La vida es bella'))
+
+const express = require('express')
+const app = express()
+const arreglo = []
+
+app.get('/home',(req,res)=>{
+    res.send("<h1>Hello World!</h1>")
+})
+app.get('/page',(req,res)=>{
+    res.send({message:"Hello World!"})
+
 })
 
-console.log(((chalk.blue)("Hola","Mundo","Irreal")))
+app.post('/user',express.json(),(req,res)=>{
+    //res.send({message:'Este es el post'})
+    arreglo.push(req,body)
+})
+
+app.get('/user',(req,res)=>{
+    res.send({users:arreglo})
+})
+
+app.get('/name',(req,res)=>{
+    //console.log(req)
+    console.log(req.body)
+    res.send()
+})
+
+app.listen(30011, () => {
+  console.log(chalk.blue('Servidor corriendo'))
+})
